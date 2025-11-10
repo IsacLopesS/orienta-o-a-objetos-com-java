69ed9cc82cb1ee3aec981ae3d201f7166193b513
isac <sisaclp15@gmail.com>
2025-11-10 17:03:04 -0300

    adicionado exercicio da pizza: classe pizza foi criada completamente e a classe CarrinhoDeCompras está incompleta

---
+++ atividade da pizza/pizza/src/CarrinhoDeCompras.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+public class CarrinhoDeCompras {
+
+   private List<Pizza> carrinho = new ArrayList<>();
+
+
+   public void adicionaPizza(Pizza pizza){
+    carrinho.add(pizza);
+   }
+    
+}
---
+++ atividade da pizza/pizza/src/Pizza.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List; // 2. Importar a interface List (boa prática)
+import java.util.HashMap;
+import java.util.Map;
+
+public class Pizza {
+
+    private List<String> listaIngredientes = new ArrayList<>();
+    private static Map<String,Integer> listaIngredientesPizzaria = new HashMap<String,Integer>();
+
+    public void adicionaIngrediente(String ingrediente){
+        listaIngredientes.add(ingrediente);
+
+        contabilizaIngrediente(ingrediente);
+    }
+
+    public int getPreco(){
+        int numeroIngredientes = listaIngredientes.size();
+
+        if(numeroIngredientes<=2){
+            return 15;
+        }
+        if(numeroIngredientes<=5){
+            return 20;
+        }
+        return 23;
+    }
+
+
+    private static void contabilizaIngrediente(String ingrediente){
+        
+        if(!listaIngredientesPizzaria.containsKey(ingrediente)){
+            listaIngredientesPizzaria.put(ingrediente, 1);
+        }else{
+            int valorAntigo = listaIngredientesPizzaria.get(ingrediente);
+            listaIngredientesPizzaria.put(ingrediente,valorAntigo+1);
+        }
+        
+
+    }
+
+
+
+}
